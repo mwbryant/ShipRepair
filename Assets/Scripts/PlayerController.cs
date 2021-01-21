ca8365d996b30cce7a55f918318bc6dedbb73c47
mwbryant <logicprojectsforfpga@gmail.com>
2021-01-21 12:25:56 -0700

    init commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float speed;
+
+    void Start() {
+
+    }
+
+    void FixedUpdate() {
+    }
+}
