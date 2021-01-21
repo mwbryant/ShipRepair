56faac00d07953a0a253304e82c923b5cab10536
mwbryant <logicprojectsforfpga@gmail.com>
2021-01-21 12:47:30 -0700

    Basic Player controller and room borders

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     public float speed;
 
-    void Start() {
+    private Rigidbody2D rb;
 
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     void FixedUpdate() {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        Vector2 totalInput = new Vector3(xInput, yInput).normalized;
+        Vector2 newPosition = rb.position + speed * totalInput;
+        rb.MovePosition(newPosition);
     }
 }
